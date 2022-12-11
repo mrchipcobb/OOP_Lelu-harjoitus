a95f77c42efc9770294a9de02b15db17860c39b5
MP Kivela <ruuhka@gmail.com>
2022-12-11 17:26:06 +0200

    Commit 2 Vieteriauto
    Luotu uusi luokka Vieteriauto ja määritelty sille perusmuodostimet ja printtimetodi Lelu-luokan pohjalta.
    
    Luon jokaisen luokan omaksi tiedostoksi.

---
+++ src/Vieteriauto.java
@@ -0,0 +1,21 @@
+
+class Vieteriauto extends Lelu{
+	
+	public Vieteriauto()
+	{
+		nimi = "";
+		paino = 0;
+	}
+	
+	public Vieteriauto(String nimi, int paino)
+	{
+		this.nimi = nimi;
+		this.paino = paino;
+	}
+	
+	@Override
+	public void printData()
+	{
+		super.printData();
+	}
+}
