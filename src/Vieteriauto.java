f7c40a332f70db95a122282f87269c982d04be3e
MP Kivela <ruuhka@gmail.com>
2022-12-11 17:36:23 +0200

    Commit 3
    Luotu Vieteriauto-luokalle attribuutti nopeus ja "kiihdyta"-metodi, sekä ylikirjoitettu "tietojen tulostus"-metodi printData

--- src/Vieteriauto.java
+++ src/Vieteriauto.java
@@ -1,21 +1,31 @@
 
 class Vieteriauto extends Lelu{
 	
+	int nopeus;
+	
 	public Vieteriauto()
 	{
 		nimi = "";
 		paino = 0;
+		nopeus = 0;
 	}
 	
-	public Vieteriauto(String nimi, int paino)
+	public Vieteriauto(String nimi, int paino, int nopeus)
 	{
 		this.nimi = nimi;
 		this.paino = paino;
+		this.nopeus = nopeus;
+	}
+	
+	public void kiihdyta()
+	{
+		nopeus++;
 	}
 	
 	@Override
 	public void printData()
 	{
 		super.printData();
+		System.out.println("Nopeus: " + nopeus + " km/h");
 	}
 }
